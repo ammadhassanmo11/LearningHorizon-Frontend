14043250861b779debfbb43f42dcddafc9141464
MaHer Abubakar <maherabubakar80@gmail.com>
2020-08-24 17:48:54 +0500

    my first commit

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
+
+//const TAVUS = React.lazy(() => import('./Demo/TAVUS/Tavus'));   //exploreTutor
+const TutorValidation = React.lazy(() => import('./Demo/LH/TutorValidation'));
+
+const exploreTutor = React.lazy(() => import('./Demo/LH/exploreTutor'));
+
+const routes = [
+
+    { path: '/dashboard/default', exact: true, name: 'Default', component: DashboardDefault },
+    { path: '/LH/TutorValidation', exact: false, name: 'TutorValidation', component: TutorValidation },  
+    { path: '/LH/exploreTutor', exact: false, name: 'exploreTutor', component: exploreTutor },  
+    
+    
+
+];
+
+export default routes;
